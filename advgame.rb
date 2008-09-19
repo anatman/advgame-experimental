750280ca11d86a2ed1974717017ccf67856f5195
clem <clemxxx@gmail.com>
2008-09-19 00:12:47 -0400

    added empty Thing class to advgame.

--- advgame.rb
+++ advgame.rb
@@ -4,3 +4,10 @@
 # prepared for the intermediate ruby class at http://rubylearning.org/class/
 # partially based on the minigame exercise from the beginning ruby class
 
+class Thing
+  # generic base class for objects, creatures, players, and places
+  
+  # a Thing has a name (string) and a description (string).
+  
+
+end

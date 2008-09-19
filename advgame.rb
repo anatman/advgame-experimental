34201787846bbd36f612287ebd2bb40a94e73d67
clem <clemxxx@gmail.com>
2008-09-19 00:07:42 -0400

    initial empty files for advgame.rb and advgame_spec.rb

---
+++ advgame.rb
@@ -0,0 +1,6 @@
+# advgame.rb
+
+# a simple skeletal adventure game programming/rspec/git/shoes exercise
+# prepared for the intermediate ruby class at http://rubylearning.org/class/
+# partially based on the minigame exercise from the beginning ruby class
+
---
+++ advgame_spec.rb
@@ -0,0 +1,6 @@
+# advgame_spec.rb
+
+# Rspec tests for a simple skeletal adventure game programming/Rspec/Git/Shoes exercise
+# prepared for the intermediate Ruby class at http://rubylearning.org/class/
+# partially based on the minigame exercise from the beginning Ruby class
+

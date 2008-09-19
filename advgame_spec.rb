56eacdb9ad24c1289feb4672e443d3a911a3bccb
clem <clemxxx@gmail.com>
2008-09-19 00:11:16 -0400

    first test. Thing class itself not started yet.

--- advgame_spec.rb
+++ advgame_spec.rb
@@ -4,3 +4,19 @@
 # prepared for the intermediate Ruby class at http://rubylearning.org/class/
 # partially based on the minigame exercise from the beginning Ruby class
 
+require 'advgame'
+
+describe Thing do
+  # make sure you have a fresh default Thing to work with for each test
+  before(:each) do
+    @thing = Thing.new
+  end
+  
+  it "should have a generic default name" do
+    @thing.name.should == "Default Name"
+  end
+  
+  after(:each) do
+    @thing = nil
+  end
+end
